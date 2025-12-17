aba192c45fc739066bb8982021267c9e47a38afa
fiodortretyakov <fiodor.tretyakov@gmail.com>
2025-12-17 16:24:36 +0000

    ¬Fixed experimental warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {}
+  rules: {},
 };
