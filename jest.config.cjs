e6960c8323486341bd7fee3a33dd3acbce843c8c
fiodortretyakov <fiodor.tretyakov@gmail.com>
2025-12-17 17:19:52 +0000

    +Added coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,8 @@ module.exports = {
     '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
   },
   extensionsToTreatAsEsm: ['.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageProvider: 'v8',
 };
